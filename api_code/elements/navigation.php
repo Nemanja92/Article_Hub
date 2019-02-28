ad510f740a890cca40b2bdfc90b421c879e2d435
Nemanja Ignjatovic <nemanja@Nemanjas-MacBook-Pro.local>
2019-02-27 22:53:05 -0500

    adding .htaccess and removing .php file extension

--- api_code/elements/navigation.php
+++ api_code/elements/navigation.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container">
-  <a class="navbar-brand" href="index.php">Article Hub</a>
+  <a class="navbar-brand" href="index">Article Hub</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -8,18 +8,18 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
+        <a class="nav-link" href="index">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">About</a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
+        <a class="nav-link" href="login">Login</a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="register.php">Register</a>
+        <a class="nav-link" href="register">Register</a>
       </li>
       
     </ul>
