824846719062f95addb74a0d07cb8a95d2525ae6
Nemanja Ignjatovic <nemanja@Nemanjas-MacBook-Pro.local>
2019-02-27 22:42:25 -0500

    adding registration and login pages

--- api_code/login.php
+++ api_code/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Article Hub | Home</title>
+    <title>Article Hub | Login</title>
     <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
@@ -15,19 +15,16 @@
 
 <!-- Navigation bar code ends -->
 
-<!-- Jumbotron code begins -->
+<!-- App form code begins -->
 
 <div class="container">
-<div class="jumbotron m-top-50">
-  <h1 class="display-4">Welcome to Article Hub!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
+<div class="col-md-8">
+<div class="app_form m-top-50">
+<h2 class="display-5">Login Here</h2>
 </div>
 </div>
-
-<!-- Jumbotron code ends -->
+</div>
+<!-- App form code ends -->
 
 <!-- Footer code starts -->
 
