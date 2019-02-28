83bccd1eeaec120d7950970ebcf68c83996e05b1
Nemanja Ignjatovic <nemanja@Nemanjas-MacBook-Pro.local>
2019-02-27 22:22:29 -0500

    adding footer, jumbotron, style.css

--- api_code/index.php
+++ api_code/index.php
@@ -6,9 +6,64 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Article Hub | Home</title>
     <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-<h2>Hello World! </h2>
+<!-- Navigation bar code begins -->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container">
+  <a class="navbar-brand" href="index.php">Article Hub</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">Register</a>
+      </li>
+      
+    </ul>
+  </div>
+  </div>
+</nav>
+
+<!-- Navigation bar code ends -->
+
+<!-- Jumbotron code begins -->
+
+<div class="container">
+<div class="jumbotron m-top-50">
+  <h1 class="display-4">Welcome to Article Hub!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
+</div>
+</div>
+
+<!-- Jumbotron code ends -->
+
+<!-- Footer code starts -->
+
+<footer class="footer">
+<div class="container">
+<span class="text-muted">Rest API Development</span>
+</div>
+</footer>
+<!-- Footer code ends -->
     <script src="assets/js/jquery.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/popper.js"></script>
