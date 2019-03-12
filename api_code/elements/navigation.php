a22d6f6fdd8583b1a22304808f036774d2f70f8c
Nemanja92 <nemanja92@icloud.com>
2019-03-11 23:14:06 -0400

    navigation support for logged in/out states

--- api_code/elements/navigation.php
+++ api_code/elements/navigation.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<div class="container">
+   <div class="container">
   <a class="navbar-brand" href="index">Article Hub</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -14,14 +14,32 @@
         <a class="nav-link" href="#">About</a>
       </li>
 
+     <?php 
+     if(isset($_SESSION['userdata']['islogged_in'])){
+      ?>
+
       <li class="nav-item">
+        <a class="nav-link" href="profile">Profile</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="logout">Logout</a>
+      </li>
+
+    <?php
+     }else{
+     ?>
+
+    <li class="nav-item">
         <a class="nav-link" href="login">Login</a>
       </li>
 
       <li class="nav-item">
         <a class="nav-link" href="register">Register</a>
       </li>
-      
+      <?php
+     }
+      ?>
     </ul>
   </div>
   </div>
